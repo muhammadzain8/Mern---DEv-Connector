811e219839e351c368545ab87221ea791f7fa459
zain <muhammadzain8@gmail.com>
2020-11-25 01:59:56 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require('express')
+
+const app = express()
+
+app.get('/', (req, res) =>{
+    res.send('Api running ')
+})
+
+const PORT=process.env.PORT || 8000
+app.listen(PORT,()=>{
+    console.log(' Server running on Port ',PORT)
+})
